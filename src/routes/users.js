8ad6b67bcb8b820c41a8337d800f7ed6fd9589f2
Samuel Ferreira <samuel.ferreira@valtech.com>
2019-05-30 13:41:06 -0300

    Devo refatorar para usar o banco de dados

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,12 @@
-module.exports = () => {
+module.exports = (app) => {
   const findAll = (req, res) => {
-    const users = [{
-      name: 'John Doe',
-      mail: 'johndoe@mail.com',
-    }];
-    res.status(200).json(users);
+    app.db('users').select()
+      .then(result => res.status(200).json(result));
   };
-  
-  const create = (req, res) => {
-    res.status(200).send(req.body);
+
+  const create = async (req, res) => {
+    const result = await app.db('users').insert(req.body, '*');
+    res.status(201).send(result[0]);
   };
 
   return { findAll, create };
